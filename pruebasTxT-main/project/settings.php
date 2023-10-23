80b31d155f36bfe3ab86321158d673d2e3648ff8
marcos1g <132932937+AgustinMarino@users.noreply.github.com>
2023-10-23 16:39:18 -0300

    a

--- pruebasTxT-main/project/settings.php
+++ pruebasTxT-main/project/settings.php
@@ -34,11 +34,15 @@ if(isset($_POST['send'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/header.scss">
+    <link rel="stylesheet" href="css/style.scss">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <?php include 'layout/header.php';?>
-    <?php include 'layout/footer.php';?>
 </head>
 <body>
-    
+  <header class="box">
+    <h1>Ajustes</h1>
+</header>
+  <?php include 'layout/footer.php';?>
+<script src="js/script.js"></script>
 </body>
 </html>
