5ac13736b2fc9d8ef5066a996bb6121987f3614d
marcos1g <mg7844428@gmail.com>
2023-10-23 14:11:56 -0300

    p

--- pruebasTxT-main/project/layout/header.php
+++ pruebasTxT-main/project/layout/header.php
@@ -21,11 +21,12 @@ if(isset($message)){
             <a href="contact.php">Contactenos</a>
             <a href="orders.php">Ordenes</a>
          </nav>
-
+<!-- iconos -->
          <div class="icons">
             <div id="menu-btn" class="fas fa-bars"></div>
             <a href="search_page.php" class="fas fa-search"></a>
             <div id="user-btn" class="fas fa-user"></div>
+            <a href="settings.php" class="fa-solid fa-gear"></a>
             <?php
                $select_cart_number = mysqli_query($conn, "SELECT * FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
                $cart_rows_number = mysqli_num_rows($select_cart_number); 
@@ -36,7 +37,7 @@ if(isset($message)){
             <p>Usuario : <span><?php echo $_SESSION['user_name']; ?></span></p>
             <p>Email : <span><?php echo $_SESSION['user_email']; ?></span></p>
             <a href="logout.php" class="delete-btn">logout</a>
-            <a href="settings.php" class="fas fa-search"></a>
+           
          </div>
       </div>
    </div>
